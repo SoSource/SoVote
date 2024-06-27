ef5973b69d6933ebef27295cc5b857f3107cf601
SoSource <Sozed@email.com>
2024-06-27 16:17:20 -0400

    initial

--- blockchain/views.py
+++ blockchain/views.py
@@ -22,16 +22,18 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def get_broadcast_list_view(request):
     print('get_broadcast_list_view')
+    obj = 'objx'
+    obj_json = 'jsonx'
     try:
         if request.method == 'POST':
             print()
-            try:
-                obj_json = json.loads(request.POST.get('obj'))
-                obj = get_or_create_model(obj_json['object_type'], obj_json)
-                broadcast_peers, broadcast_list, validator_list = get_broadcast_peers(obj)
-                return JsonResponse({'obj' : obj, 'broadcast_list' : broadcast_list, 'validator_list' : validator_list})
-            except:
-                return JsonResponse({'obj' : obj, 'broadcast_list' : [], 'validator_list' : []})
+            # try:
+            obj_json = json.loads(request.POST.get('obj'))
+            obj = get_or_create_model(obj_json['object_type'], obj_json)
+            broadcast_peers, broadcast_list, validator_list = get_broadcast_peers(obj)
+            return JsonResponse({'obj' : obj, 'broadcast_list' : broadcast_list, 'validator_list' : validator_list})
+            # except:
+            #     return JsonResponse({'obj' : obj, 'broadcast_list' : [], 'validator_list' : []})
         else:
             return JsonResponse({'message' : 'not post'})
     except Exception as e:
@@ -39,7 +41,7 @@ def get_broadcast_list_view(request):
             x = request.POST.get('obj')
         except Exception as x:
             x = str(x)
-        return JsonResponse({'message' : str(e) + '--' + x})
+        return JsonResponse({'message' : str(e) + obj + '//' + obj_json + '--' + x})
 
 
 @csrf_exempt
